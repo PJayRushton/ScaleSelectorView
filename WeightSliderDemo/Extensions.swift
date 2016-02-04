7133ac08e5309c1c7d32b9b086f54fdbb45dd97d
PJayRushton <parker.rushton@octanner.com>
2016-02-04 13:52:13 -0700

    Adds corner radius extension

--- WeightSliderDemo/Extensions.swift
+++ WeightSliderDemo/Extensions.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+import UIKit
+
+@IBDesignable extension UIView {
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+}
