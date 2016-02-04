8a57a84bf75d77d4b3fde1edd462522c1be5f496
PJayRushton <parker.rushton@octanner.com>
2016-02-04 14:59:12 -0700

    Configures WeightLogViewController to be more reusable

--- WeightSliderDemo/WeightLogViewController.swift
+++ WeightSliderDemo/WeightLogViewController.swift
@@ -12,8 +12,13 @@ class WeightLogViewController: UIViewController {
 
     @IBOutlet weak var weightLabel: UILabel!
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var indicatorLineView: UIView!
     
-    private let maxWeight = 400
+    private let weightDefaultsKey = "UserWeight"
+    private let maxWeight = 463
+    private let itemWidth: CGFloat = 14.0
+    private let evenLineHeight: CGFloat = 30.0
+    private let oddLineHeight: CGFloat = 15.0
     
     private var weight: Int = 150 {
         didSet {
@@ -24,13 +29,12 @@ class WeightLogViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let indexPath = NSIndexPath(forItem: 150, inSection: 0)
-        collectionView.scrollToItemAtIndexPath(indexPath, atScrollPosition: UICollectionViewScrollPosition.CenteredHorizontally, animated: false)
-        configureWeight()
+        setInitalWeight()
     }
 
     @IBAction func saveWeightButtonPressed(sender: UIButton) {
-        print("Weight logged: \(weight)")
+        NSUserDefaults.standardUserDefaults().setInteger(weight, forKey: weightDefaultsKey)
+        print("Saved Weight! (\(weight))")
     }
 
 }
@@ -53,18 +57,27 @@ extension WeightLogViewController: UIScrollViewDelegate {
 
 private extension WeightLogViewController {
     
+    private func setInitalWeight() {
+        let savedWeight = NSUserDefaults.standardUserDefaults().integerForKey(weightDefaultsKey)
+        if savedWeight != 0 {
+            weight = savedWeight
+        }
+        let indexPath = NSIndexPath(forItem: weight, inSection: 0)
+        collectionView.scrollToItemAtIndexPath(indexPath, atScrollPosition: UICollectionViewScrollPosition.CenteredHorizontally, animated: false)
+    }
+    
     private func snapToLine() {
         guard let centerIndex = indexPathForCellAtCenter() else { return }
         collectionView.scrollToItemAtIndexPath(centerIndex, atScrollPosition: UICollectionViewScrollPosition.CenteredHorizontally, animated: true)
     }
     
     private func configureWeight() {
-        guard let centerIndex = indexPathForCellAtCenter() else { return }
-        weight = centerIndex.item
+        guard let centerItemIndexPath = indexPathForCellAtCenter() else { return }
+        weight = centerItemIndexPath.item
     }
     
     private func indexPathForCellAtCenter() -> NSIndexPath? {
-        let centerPoint = view.convertPoint(view.center, toView: collectionView)
+        let centerPoint = view.convertPoint(indicatorLineView.center, toView: collectionView)
         return collectionView.indexPathForItemAtPoint(centerPoint)
     }
     
@@ -74,7 +87,7 @@ extension WeightLogViewController: UICollectionViewDataSource, UICollectionViewD
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCellWithReuseIdentifier(WeightLineCollectionViewCell.reuseIdentifier, forIndexPath: indexPath) as? WeightLineCollectionViewCell else { preconditionFailure() }
-        cell.heightConstraint.constant = indexPath.item % 2 == 0 ? 30.0 : 15.0
+        cell.heightConstraint.constant = indexPath.item % 2 == 0 ? evenLineHeight : oddLineHeight
         
         return cell
     }
@@ -87,7 +100,7 @@ extension WeightLogViewController: UICollectionViewDataSource, UICollectionViewD
 extension WeightLogViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-        return CGSizeMake(14, collectionView.bounds.height)
+        return CGSizeMake(itemWidth, collectionView.bounds.height)
     }
     
 }
